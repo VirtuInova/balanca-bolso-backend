93b2e5be99d6d75fe220c9792358e009cf3ff42f
landerson98n <landerson98n@gmail.com>
2024-04-01 14:05:32 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   );
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT, "0.0.0.0");
 }
 bootstrap();
