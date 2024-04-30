e5a3f6b757738face85413909f7c08bae838e6d5
landerson98n <landerson98n@gmail.com>
2024-04-30 11:54:04 -0300

    database railway

--- src/shared/database/prisma.service.ts
+++ src/shared/database/prisma.service.ts
@@ -1,9 +1,14 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { withPulse } from '@prisma/extension-pulse'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    const prisma = new PrismaClient()
+    const prisma = new PrismaClient().$extends(
+      withPulse({
+        apiKey: process.env['PULSE_API_KEY'] as string
+      })
+    )
   }
 }
